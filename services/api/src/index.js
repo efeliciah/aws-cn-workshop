94fed1dcddc575c2cd454b31459dd07b874f3348
Felicia Hägerström <103430357+efeliciah@users.noreply.github.com>
2024-04-04 11:31:01 +0000

    commited

--- services/api/src/index.js
+++ services/api/src/index.js
@@ -8,6 +8,9 @@ app.use('/', routes);
 
 // ...
 // Healthcheck
+app.get("/healthz", (req, res) => {
+    res.send({ success: true, message: "200 (OK)" });
+});
 
 const server = app.listen(env.port, () => {
     console.log(`Listening on port ${env.port}`)
@@ -15,3 +18,12 @@ const server = app.listen(env.port, () => {
 
 // ...
 // Shut down gracefully
+
+process.on('SIGTERM', () => {
+    console.log('Closing idle connections...')
+    server.closeIdleConnections()
+    console.log('The service is about to shut down!');
+    
+    // Finish any outstanding requests, then...
+    process.exit(0); 
+});
